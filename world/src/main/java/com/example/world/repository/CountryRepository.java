18b49ec52f353a9c8efb80dabff47e5aee3fc7ce
deepcloudlabs <deepcloudlabs@gmail.com>
2022-02-18 17:00:11 +0300

    Update CountryRepository.java

--- world/src/main/java/com/example/world/repository/CountryRepository.java
+++ world/src/main/java/com/example/world/repository/CountryRepository.java
@@ -9,6 +9,6 @@ import com.example.world.document.CountryDocument;
 
 public interface CountryRepository extends MongoRepository<CountryDocument, String>{
 	List<CountryDocument> findAllByContinent(String continent);
-    @Aggregation(pipeline="{ '$group' :  {'_id': 'continent'} }")
+    @Aggregation(pipeline="{ '$group' :  {'_id': '$continent'} }")
 	List<String> getAllContinents();
 }
