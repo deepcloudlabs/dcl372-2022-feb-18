bfc757539db47ffd77b45a5ec7f9658965eaaddc
deepcloudlabs <deepcloudlabs@gmail.com>
2022-02-18 14:13:02 +0300

    Update StandardLotteryService.java

--- study-multiple-implementations-in-spring/src/com/example/lottery/service/business/StandardLotteryService.java
+++ study-multiple-implementations-in-spring/src/com/example/lottery/service/business/StandardLotteryService.java
@@ -1,28 +1,33 @@
 package com.example.lottery.service.business;
 
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.IntStream;
 
 import org.springframework.stereotype.Service;
 
 import com.example.lottery.service.LotteryService;
 import com.example.lottery.service.RandomNumberGeneratorService;
-import com.example.lottery.service.RandomNumberServiceQuality;
-import com.example.lottery.service.ServiceQualityLevel;
 
 @Service
 public class StandardLotteryService implements LotteryService {
 
-	private RandomNumberGeneratorService randomNumberGeneratorService;
+	private List<RandomNumberGeneratorService> 
+	                          randomNumberGeneratorServices;
+	private AtomicInteger counter = new AtomicInteger();
 	
 	public StandardLotteryService(
-			@RandomNumberServiceQuality(ServiceQualityLevel.FAST)
-			RandomNumberGeneratorService randomNumberGeneratorService) {
-		this.randomNumberGeneratorService = randomNumberGeneratorService;
+			//@RandomNumberServiceQuality(ServiceQualityLevel.FAST)
+	List<RandomNumberGeneratorService> randomNumberGeneratorServices) {
+	    this.randomNumberGeneratorServices = randomNumberGeneratorServices;
 	}
 
 	@Override
 	public List<Integer> draw(int max, int size) {
+		var index = counter.getAndIncrement() % 
+				randomNumberGeneratorServices.size();
+		var randomNumberGeneratorService = 
+				randomNumberGeneratorServices.get(index);
 		return IntStream.generate(() -> 
 		       randomNumberGeneratorService.generate(1, max))
 				        .distinct()
